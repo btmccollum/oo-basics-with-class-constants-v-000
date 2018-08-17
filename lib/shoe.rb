58286b4580baf917086ba5689bcaad30bb74585f
Brad McCollum <btmcco8@gmail.com>
2018-08-17 17:10:05 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -9,6 +9,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
     BRANDS << brand
+    puts BRAND.uniq
     # binding.pry
   end
 
@@ -17,6 +18,5 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-puts BRANDS.uniq
 
 end
