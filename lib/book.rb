1eec59a5738b1ece76169cca9d65352d5b2e96d2
Brad McCollum <btmcco8@gmail.com>
2018-08-17 16:47:38 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,9 @@
 class Book
-  GENRES = []
-  attr_accessor :author, :page_count, 
+  attr_accessor :author, :page_count,
   attr_reader :title, :genre
 
+  GENRES = []
+  
   def initialize(title)
     @title = title
   end
